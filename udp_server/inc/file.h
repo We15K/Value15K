205fc03e5de3dd9d67de909cee769f72223b2603
mengc0508 <mengchangpc@outlook.com>
2023-02-27 22:13:28 +0800

    计算文件大小

--- udp_server/inc/file.h
+++ udp_server/inc/file.h
@@ -14,6 +14,8 @@ public:
     // 列出目录下的文件
     int FileList();
 
+    long GetFileSize(int fileIndex);
+
 private:
     // 目录流指针
     DIR *m_dir;
@@ -21,8 +23,12 @@ private:
     // 保存最后一次打开文件的文件描述符
     int m_fd;
 
+    // 保存打开文件的指针
+    FILE *m_file;
+
     // 指向的目录流中的下一个目录项
     struct dirent *m_dirInfo;
 
     std::map<int, std::string> m_fileName;
+    int m_fileNum;
 };
